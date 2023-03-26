b031abeba11bc342316b5caf4e911f0102ddd155
Sebastian <Sebastian>
2023-03-25 22:22:06 -0500

    database migration

--- db/models/Book.php
+++ db/models/Book.php
@@ -3,36 +3,39 @@ require_once(str_replace('models', 'Db.php', __DIR__));
 require_once(str_replace('Book.php', 'interfaces.php', __FILE__));
 
 class Book extends Db implements Models {
-  public function __construct() {
-    $connection = $this->connect();
+  public $connection;
+  public static $created = false;
 
-    mysqli_query($connection, "
+  public function __construct() {
+    $this->connection = $this->connect();
+
+    mysqli_query($this->connection, "
       CREATE TABLE IF NOT EXISTS book (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(255),
-        description VARCHAR(655),
-        link_image VARCHAR(800)
+        id INT NOT NULL AUTO_INCREMENT ,
+        uuid VARCHAR(255) NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        description VARCHAR(800) NOT NULL,
+        link_image VARCHAR(1200) NOT NULL,
+        PRIMARY KEY (id)
       );
     ");
   }
 
   public function insert(...$data) {
     [$name, $description, $link_image] = $data;
-    $connection = $this->connect();
 
-    $name = mysqli_real_escape_string($connection, $name);
-    $description = mysqli_real_escape_string($connection, $description);
-    $link_image = mysqli_real_escape_string($connection, $link_image);
+    $name = mysqli_real_escape_string($this->connection, $name);
+    $description = mysqli_real_escape_string($this->connection, $description);
+    $link_image = mysqli_real_escape_string($this->connection, $link_image);
 
-    mysqli_query($connection, "
-      INSERT INTO book (name, description, link_image) VALUES 
-      ('$name', '$description', '$link_image');
+    mysqli_query($this->connection, "
+      INSERT INTO book (uuid, name, description, link_image) VALUES 
+      (UUID_SHORT(), '$name', '$description', '$link_image');
     ");
   }
 
   public function get_all() {
-    $connection = $this->connect();
-    $result = mysqli_query($connection, "SELECT * FROM book;");
+    $result = mysqli_query($this->connection, "SELECT * FROM book;");
 
     return $result;
   }
